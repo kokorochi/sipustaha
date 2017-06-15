e79b0a70cf22a950040825b7edd415afb507152b
Surya Wijaya <kokorochi.zhou@gmail.com>
2017-06-15 10:38:11 +0700

    Add homecontroller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller {
+    public function __construct()
+    {
+        $this->middleware('isauth');
+    }
+
+    public function index()
+    {
+        return 'home page';
+    }
+}
