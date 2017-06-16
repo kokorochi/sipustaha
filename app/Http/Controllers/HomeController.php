ed1fb233c4381b1af8f7a5107997daac943ec3ad
Surya Wijaya <kokorochi.zhou@gmail.com>
2017-06-16 20:02:20 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,16 +8,40 @@ use Illuminate\Support\Facades\Redirect;
 class HomeController extends Controller {
     public function __construct()
     {
-        $this->middleware('isauth');
+//        $this->middleware('isauth');
     }
 
     public function index()
     {
-        return 'home page';
-    }
+        $client = new \GuzzleHttp\Client();
 
-    public function login()
-    {
-        Redirect::to('akun.usu.ac.id');
+        $response = $client->request('POST', 'https://akun.usu.ac.id/auth/login/apps', [
+            'form_params' => [
+                'identity' => '88072116081001"',
+                'password' => '123qweasd',
+                'random_char' => 'TVWBJBSuwyewbwgcuw23657438zs'
+            ]
+        ]);
+
+        dd(json_decode($response->getBody()));
+
+//        curl_setopt_array($curl, array(
+//            CURLOPT_RETURNTRANSFER => TRUE,
+//            CURLOPT_URL => 'https://akun.usu.ac.id/auth/login/apps',
+//            CURLOPT_POST => 1,
+//            CURLOPT_POSTFIELDS => array("identity" => "141402072994031003", "password" => "test", "random_char" => "TVWBJBSuwyewbwgcuw23657438zs",
+//                                        CURLOPT_HEADER => TRUE
+//            )));
+
+
+
+//        curl_setopt_array($curl, array(
+//            CURLOPT_RETURNTRANSFER => TRUE,
+//            CURLOPT_URL => 'https://akun.usu.ac.id/auth/listen',
+//            CURLOPT_POST => 1,
+//            CURLOPT_POSTFIELDS => array("Authorization" => "Bearer token", "password" => "test", "random_char" => "TVWBJBSuwyewbwgcuw23657438zs",
+//                                        CURLOPT_HEADER => TRUE
+//            )));
+//        return 'home page';
     }
 }
